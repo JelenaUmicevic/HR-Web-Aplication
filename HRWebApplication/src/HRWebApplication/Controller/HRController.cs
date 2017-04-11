4b497cc074a2ba827edb70425084e4b06a5063f8
HR_WEB_APPLICATION <jela.pezos@gmail.com>
2017-04-11 21:26:56 +0200

    za sad

---
+++ HRWebApplication/src/HRWebApplication/Controller/HRController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRWebApplication.Controller
+{
+    public class HRController : Controller
+    {
+        public JsonResult GetUsers();
+    }
+
+}
