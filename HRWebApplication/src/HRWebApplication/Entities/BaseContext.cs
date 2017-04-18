02f6ef14228c1c5c1aac8e1f864637f109bfd602
HR_WEB_APPLICATION <jela.pezos@gmail.com>
2017-04-18 19:47:54 +0200

    entities

---
+++ HRWebApplication/src/HRWebApplication/Entities/BaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRWebApplication.Entities
+{
+    public class BaseContext : DbContext
+    {
+
+    }
+}
